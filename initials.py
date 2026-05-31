229f0461e50e3863e1ee5eccc6697ed6c7a5a956
hyunsuyoon17 <80530544+hyunsuyoon17@users.noreply.github.com>
2026-05-31 18:11:57 +0000

    initials
    Implement a function to print initials in stylized letters.

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Print out my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Hyunsu Yoon
+# STUDENT ID: 2425400522
+# Email: hyunsuyoon17@gmail.com
+# Grader: Carolyn
+#
+# On my honor, HYUNSU YOON, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in large stylized letters
 
+    print()
+    print("...HSY")
+    print()
+
+    print("...HH........HH..........SSSSSSSSS.........YY        YY...")
+    print("...HH........HH........SSSSSSSSSSS..........YY      YY....")
+    print("...HH........HH........SS....................YY    YY.....")
+    print("...HH........HH........SS.....................YY  YY......")
+    print("...HHHHHHHHHHHH...........SSSSSS...............YYYY.......")
+    print("...HHHHHHHHHHHH............SSSSSS...............YY........")
+    print("...HH........HH................SSSS.............YY........")
+    print("...HH........HH................SSSS.............YY........")
+    print("...HH........HH...**...SSSSSSSSSSS......**......YY......**")
+    print("...HH........HH...**...SSSSSSSSSSS......**......YY......**")
+
+    print()
 
 
 main()
